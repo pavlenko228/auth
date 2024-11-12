6239df28257c783704905d82935e02bcec186405
Pavlenko Mihail <miapavlenko@mai.education>
2024-11-12 11:03:30 +0300

    delete needless fields naming_convention

--- core/models/base.py
+++ core/models/base.py
@@ -1,6 +1,5 @@
 from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import declared_attr
 
 from core.config import settings
 
@@ -8,6 +7,3 @@ from core.config import settings
 class Base(DeclarativeBase):
     __abstract__ = True
 
-    metadata = MetaData(
-        naming_convention=settings.db.naming_convention,
-    )
