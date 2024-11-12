9da0a9d15afe37d20c5fed7e5152def66a8b6f08
Pavlenko Mihail <miapavlenko@mai.education>
2024-11-12 16:54:36 +0300

    update users.py

--- api/api_v1/users.py
+++ api/api_v1/users.py
@@ -1,10 +1,9 @@
 #создание, удаление, чтение пользователей
-from fastapi import APIRouter, Depends, HTTPException, requests
+import httpx
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import HTTPBearer
-
 from sqlalchemy.ext.asyncio import AsyncSession
 import uuid
-
 
 from api.api_v1.utils import create_access_token, create_refresh_token, hash_password
 from core.config import settings
@@ -13,9 +12,7 @@ from core.models.auth import Auth
 from core.schemas import UserCreate
 from core.schemas.jwt import TokenInfo
 
-
 http_bearer = HTTPBearer(auto_error=False)
-
 
 router = APIRouter(
     prefix=settings.api.v1.users,
@@ -23,27 +20,26 @@ router = APIRouter(
     dependencies=[Depends(http_bearer)],
 )
 
-
 @router.post("/create_user", response_model=TokenInfo)
 async def create_user(
     user: UserCreate,
     db: AsyncSession = Depends(db_helper.session_getter)
 ):
-    uuid = str(uuid.uuid4())
+    user_uuid = str(uuid.uuid4())
     email = user.email
     hashed_password = hash_password(user.password)
 
-    new_user = Auth(uuid=uuid, email=email, hashed_password=hashed_password)
+    new_user = Auth(uuid=user_uuid, email=email, hashed_password=hashed_password)
 
     try:
         db.add(new_user)
         await db.commit()
     except Exception as e:
         await db.rollback()
-        raise HTTPException(status_code=500, detail=f"Error saving user: {str(e)}")
+        raise HTTPException(status_code=400, detail=f"Database error: {str(e)}")
 
-    access_token = create_access_token(data={"uuid": uuid, "scope": "create_user"})
-    refresh_token = create_refresh_token(data={"uuid": uuid})
+    access_token = create_access_token(data={"uuid": user_uuid, "scope": "create_user"})
+    refresh_token = create_refresh_token(data={"uuid": user_uuid})
 
     user_service_url = "http://user_service_host/create_user"
 
@@ -52,8 +48,10 @@ async def create_user(
     }
 
     try:
-        response = requests.post(user_service_url, json={"uuid": uuid}, headers=headers)
-        response.raise_for_status()
-    except requests.exceptions.RequestException as e:
-        raise HTTPException(status_code=500, detail="Error calling user service")
+        async with httpx.AsyncClient() as client:
+            response = await client.post(user_service_url, json={"uuid": user_uuid}, headers=headers)
+            response.raise_for_status()  # Проверка на ошибки HTTP
+    except httpx.RequestError as e:
+        raise HTTPException(status_code=500, detail=f"Error calling user service: {str(e)}")
+
     return TokenInfo(access_token=access_token, refresh_token=refresh_token)
