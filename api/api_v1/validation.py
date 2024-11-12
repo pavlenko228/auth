1230d6ae9ba1f4f90fc899ae7b095aeebe06a773
Pavlenko Mihail <miapavlenko@mai.education>
2024-11-12 14:41:16 +0300

    initial creation code in validation

--- api/api_v1/validation.py
+++ api/api_v1/validation.py
@@ -1,1 +1,125 @@
-#валидация токенов, получение их payload, 
+#валидация токенов, получение их payload
+
+#подумать над тем, что нужно возвращать в validate_token_type при ошибке
+
+from fastapi import Depends, HTTPException, Form
+from fastapi.security import OAuth2PasswordBearer
+from jwt import InvalidTokenError
+from starlette import status
+
+from api_v1.utils import (
+    TOKEN_TYPE_FIELD,
+    ACCESS_TOKEN_TYPE,
+    REFRESH_TOKEN_TYPE,
+)
+import utils 
+from core.schemas.user import CreateUser
+
+
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="/api/v1/jwt-auth/login/",
+)
+
+
+def get_current_token_payload(
+    token: str = Depends(oauth2_scheme),
+) -> dict:
+    try:
+        payload = utils.decode_jwt(
+            token=token,
+        )
+    except InvalidTokenError as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"invalid token error: {e}",
+        )
+    return payload
+
+
+def validate_token_type(
+    payload: dict,
+    token_type: str,
+) -> bool:
+    current_token_type = payload.get(TOKEN_TYPE_FIELD)
+    if current_token_type == token_type:
+        return True
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail=f"invalid token type {current_token_type!r} expected {token_type!r}",
+    )
+
+#нужен ли мне вообще этот метод
+users_db = {}  #не забудь обновить 
+def get_user_by_token_sub(payload: dict) -> CreateUser:
+    uuid: str | None = payload.get("sub")
+    if  user := users_db.get(uuid): #это надо преределать, чтобы обращаться к настоящей бд
+        return user
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="token invalid (user not found)",
+    )
+
+
+def get_auth_user_from_token_of_type(token_type: str):
+    def get_auth_user_from_token(
+        payload: dict = Depends(get_current_token_payload),
+    ) -> CreateUser:
+        validate_token_type(payload, token_type)
+        return get_user_by_token_sub(payload)
+
+    return get_auth_user_from_token
+
+
+class UserGetterFromToken:
+    def __init__(self, token_type: str):
+        self.token_type = token_type
+
+    def __call__(
+        self,
+        payload: dict = Depends(get_current_token_payload),
+    ):
+        validate_token_type(payload, self.token_type)
+        return get_user_by_token_sub(payload)
+
+
+# get_current_auth_user = UserGetterFromToken(ACCESS_TOKEN_TYPE)
+get_current_auth_user = get_auth_user_from_token_of_type(ACCESS_TOKEN_TYPE)
+
+get_current_auth_user_for_refresh = UserGetterFromToken(REFRESH_TOKEN_TYPE)
+
+
+def get_current_active_auth_user(
+    user: CreateUser = Depends(get_current_auth_user),
+):
+    if user.active:
+        return user
+    raise HTTPException(
+        status_code=status.HTTP_403_FORBIDDEN,
+        detail="inactive user",
+    )
+
+
+def validate_auth_user(
+    username: str = Form(),
+    password: str = Form(),
+):
+    unauthed_exc = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="invalid username or password",
+    )
+    if not (user := users_db.get(username)):
+        raise unauthed_exc
+
+    if not utils.validate_password(
+        password=password,
+        hashed_password=user.password,
+    ):
+        raise unauthed_exc
+
+    if not user.active:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="user inactive",
+        )
+
+    return user
