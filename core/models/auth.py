37df0fe8edecb7f84dc97bcec3e1398274831d99
Pavlenko Mihail <miapavlenko@mai.education>
2024-11-12 15:40:40 +0300

    rename user.py to auth.py

--- core/models/auth.py
+++ core/models/auth.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 from .base import Base
 
 
-class User(Base):
+class Auth(Base):
     __tablename__ = 'auth'
 
     uuid: Mapped[str] = mapped_column(String, unique=True, primary_key=True) #default=str(uuid.uuid4())
