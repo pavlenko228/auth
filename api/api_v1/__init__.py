c1e9de32dc23ead60e68d549ac3017df8acb8e5d
Pavlenko Mihail <miapavlenko@mai.education>
2024-11-12 12:17:25 +0300

    rename router parameters

--- api/api_v1/__init__.py
+++ api/api_v1/__init__.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from .jwt_auth import router as jwt_auth_router
-from .views import router as views_router
+from .users import router as users_router
 
 from core.config import settings
 
@@ -9,4 +9,4 @@ router = APIRouter(
     prefix=settings.api.v1.prefix,
 )
 router.include_router(jwt_auth_router)
-router.include_router(views_router)
+router.include_router(users_router)
