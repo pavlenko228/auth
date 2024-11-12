7c95dec001af59d19239ad910c6278b12389fe6d
Pavlenko Mihail <miapavlenko@mai.education>
2024-11-12 13:21:12 +0300

    an incomplete update utils

--- api/api_v1/utils.py
+++ api/api_v1/utils.py
@@ -3,7 +3,7 @@
 
 
 #проверить нужно ли вообще expire_timedelta: timedelta | None = None
-import uuid
+#!!!!!!create_access_token продумать, что приходит в функцию
 from datetime import datetime, timedelta
 
 import bcrypt
@@ -11,6 +11,7 @@ import jwt
 
 from core.config import settings
 
+from core.schemas.user import CreateUser
 
 def encode_jwt(
     payload: dict,
@@ -47,6 +48,50 @@ def decode_jwt(
         algorithms=[algorithm],
     )
     return decoded
+
+
+TOKEN_TYPE_FIELD = "type"
+ACCESS_TOKEN_TYPE = "access"
+REFRESH_TOKEN_TYPE = "refresh"
+
+
+def create_jwt(
+    token_type: str,
+    token_data: dict,
+    expire_minutes: int = settings.auth_jwt.access_token_expire_minutes,
+    expire_timedelta: timedelta | None = None,
+) -> str:
+    jwt_payload = {TOKEN_TYPE_FIELD: token_type}
+    jwt_payload.update(token_data)
+    return encode_jwt(
+        payload=jwt_payload,
+        expire_minutes=expire_minutes,
+        expire_timedelta=expire_timedelta,
+    )
+
+
+def create_access_token(user: CreateUser) -> str:
+    jwt_payload = {
+        "sub": user.username,
+        "username": user.username,
+        "email": user.email,
+    }
+    return create_jwt(
+        token_type=ACCESS_TOKEN_TYPE,
+        token_data=jwt_payload,
+        expire_minutes=settings.auth_jwt.access_token_expire_minutes,
+    )
+
+
+def create_refresh_token(user: CreateUser) -> str:
+    jwt_payload = {
+        "sub": user.username,
+    }
+    return create_jwt(
+        token_type=REFRESH_TOKEN_TYPE,
+        token_data=jwt_payload,
+        expire_timedelta=timedelta(days=settings.auth_jwt.refresh_token_expire_days),
+    )
 
 
 def hash_password(
